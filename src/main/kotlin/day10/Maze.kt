4692037f2e91d1f28d91aa85fca51d6474bf9d0b
CreativeQuality <info@creativequality.dev>
2023-12-12 06:23:54 +0100

    refactor day 10 code a bit

---
+++ src/main/kotlin/day10/Maze.kt
@@ -0,0 +1,79 @@
+package day10
+
+import day03.Coordinate
+
+class Maze(originalTiles: Map<Coordinate, Char>) {
+
+    companion object Factory {
+        fun fromString(seq: Sequence<String>): Maze =
+            Maze(seq.flatMapIndexed { y, s -> s.mapIndexed { x, c -> Pair(Coordinate(x, y), c) } }.toMap())
+    }
+
+    val tiles: Map<Coordinate, Char>
+    val inferredStartValue: Char
+    val northWestCorner: Coordinate = originalTiles.minBy { (k, _) -> k.x + k.y }.key
+
+    private val startNode: Coordinate = originalTiles.filterValues { it == 'S' }.keys.singleOrNull() ?: northWestCorner
+    private val southEastCorner: Coordinate = originalTiles.maxBy { (k, _) -> k.x + k.y }.key
+    private val boxDrawing: Map<Char, Char> =
+        mapOf(
+            Pair('|', '│'),
+            Pair('-', '─'),
+            Pair('L', '└'),
+            Pair('J', '┘'),
+            Pair('7', '┐'),
+            Pair('F', '┌'),
+            Pair('.', '░')
+        )
+
+    init {
+        tiles = originalTiles.map { (k, v) -> Pair(k, boxDrawing.getOrDefault(v, v)) }.toMap()
+        inferredStartValue = boxDrawing.values.first {
+            connections(startNode, it).all { neighbor ->
+                tiles.containsKey(neighbor) && connections(neighbor).contains(startNode)
+            }
+        }
+    }
+
+    fun hasNode(node: Coordinate): Boolean = tiles.containsKey(node)
+
+    fun hasTile(node: Coordinate, value: Char): Boolean = hasNode(node) && tiles[node]!! == value
+
+    fun findLoop(): List<Coordinate> {
+        tailrec fun findIt(node: Coordinate, path: MutableList<Coordinate> = mutableListOf()): List<Coordinate> {
+            path.add(node)
+            val next = connections(node).filter { !path.contains(it) }
+            return if (next.isEmpty()) path
+            else findIt(next.first(), path)
+        }
+        return findIt(startNode)
+    }
+
+    fun print() {
+        for (y in northWestCorner.x..southEastCorner.y) {
+            var line = ""
+            for (x in northWestCorner.y..southEastCorner.x) {
+                line += tiles[Coordinate(x, y)] ?: ' '
+            }
+            println(line)
+        }
+        println()
+    }
+
+    private fun connections(
+        n: Coordinate,
+        value: Char = tiles[n] ?: throw RuntimeException("Invalid node value!")
+    ): Set<Coordinate> =
+        when (value) {
+            '│' -> setOf(n.north(), n.south())
+            '─' -> setOf(n.east(), n.west())
+            '└' -> setOf(n.north(), n.east())
+            '┘' -> setOf(n.north(), n.west())
+            '┐' -> setOf(n.south(), n.west())
+            '┌' -> setOf(n.south(), n.east())
+            '░' -> setOf()
+            'S' -> connections(n, inferredStartValue)
+            else -> throw RuntimeException("Invalid node value!")
+        }
+}
+
--- src/main/kotlin/day10/PipeMaze.kt
+++ src/main/kotlin/day10/PipeMaze.kt
@@ -5,82 +5,35 @@ import day03.Coordinate
 
 class PipeMaze(resource: String = "day10/input.txt") : KtPuzzle(resource) {
 
-    private val boxDrawing: Map<Char, Char> =
-        mapOf(
-            Pair('|', '│'),
-            Pair('-', '─'),
-            Pair('L', '└'),
-            Pair('J', '┘'),
-            Pair('7', '┐'),
-            Pair('F', '┌'),
-            Pair('.', '░')
-        )
-
-    private val maze: Map<Coordinate, Char> = sequenceInput().flatMapIndexed { y, s ->
-        s.mapIndexed { x, c -> Pair(Coordinate(x, y), boxDrawing.getOrDefault(c, c)) }
-    }.toMap()
-
     override fun firstStar(): Int {
-        print(maze)
-        val start = maze.filterValues { it == 'S' }.keys.single()
-        val loop = findLoop(start, maze)
+        val maze = Maze.fromString(sequenceInput())
+        val loop = maze.findLoop()
         return loop.size / 2
     }
 
     override fun secondStar(): Int {
-        print(maze)
-        val enlargedMaze = enlargeMaze()
-        val start = enlargedMaze.filterValues { it == 'S' }.keys.single()
-        val loop = findLoop(start, enlargedMaze)
-        val updatedEnlargedMaze = enlargedMaze.map { (k,v) -> if (loop.contains(k)) Pair(k, v) else Pair(k, '░') }.toMap()
-        print(updatedEnlargedMaze)
-        val reachable = findReachable(updatedEnlargedMaze)
-        print(reachable.associateWith { '░' })
+        val maze = Maze.fromString(sequenceInput())
+        maze.print()
+        val enlargedMaze = enlargeMaze(maze)
+        val loop = enlargedMaze.findLoop()
+        val updatedEnlargedMaze =
+            Maze(enlargedMaze.tiles.map { (k, v) -> if (loop.contains(k)) Pair(k, v) else Pair(k, '░') }.toMap())
+        updatedEnlargedMaze.print()
+        val reachable = Maze(findReachable(updatedEnlargedMaze).associateWith { '░' })
+        reachable.print()
         val enclosedTiles =
-            updatedEnlargedMaze.filter { it.value == '░' && !reachable.contains(it.key) && it.key.x % 2 == 0 && it.key.y % 2 == 0 }
+            updatedEnlargedMaze.tiles.filter { it.value == '░' && !reachable.hasNode(it.key) && it.key.x % 2 == 0 && it.key.y % 2 == 0 }
         return enclosedTiles.size
     }
 
-    private fun findLoop(start: Coordinate, maze: Map<Coordinate, Char>): List<Coordinate> {
-        tailrec fun findIt(node: Coordinate, path: MutableList<Coordinate> = mutableListOf()): List<Coordinate> {
-            path.add(node)
-            val next = connections(node, maze).filter { !path.contains(it) }
-            return if (next.isEmpty()) path
-            else findIt(next.first(), path)
-        }
-        return findIt(start)
-    }
-
-    private fun connections(
-        n: Coordinate,
-        maze: Map<Coordinate, Char>,
-        value: Char = maze[n] ?: throw RuntimeException("Invalid node value!")
-    ): Set<Coordinate> =
-        when (value) {
-            '│' -> setOf(n.north(), n.south())
-            '─' -> setOf(n.east(), n.west())
-            '└' -> setOf(n.north(), n.east())
-            '┘' -> setOf(n.north(), n.west())
-            '┐' -> setOf(n.south(), n.west())
-            '┌' -> setOf(n.south(), n.east())
-            'S' -> connections(n, maze, inferPipe(n, maze))
-            '░' -> setOf()
-            else -> throw RuntimeException("Invalid node value!")
-        }
-
-    private fun inferPipe(node: Coordinate, maze: Map<Coordinate, Char>): Char =
-        boxDrawing.values.first {
-            connections(node, maze, it).all { neighbor -> maze.containsKey(neighbor) && connections(neighbor, maze).contains(node) }
-        }
-
-    private fun findReachable(maze: Map<Coordinate, Char>): Set<Coordinate> {
+    private fun findReachable(maze: Maze): Set<Coordinate> {
         tailrec fun find(
             current: Coordinate,
             planned: MutableList<Coordinate> = mutableListOf(),
             reachable: MutableList<Coordinate> = mutableListOf()
         ): List<Coordinate> {
             val neighbors = current.adjacentStraightCoordinates()
-                .filter { maze.containsKey(it) && maze[it]!! == '░' && !planned.contains(it) && !reachable.contains(it) }
+                .filter { maze.hasTile(it, '░') && !planned.contains(it) && !reachable.contains(it) }
             reachable.add(current)
             planned.addAll(neighbors)
             if (planned.isEmpty()) return reachable
@@ -90,32 +43,28 @@ class PipeMaze(resource: String = "day10/input.txt") : KtPuzzle(resource) {
                 return find(next, planned, reachable)
             }
         }
-
-        val northWestCorner = maze.minBy { (k, _) -> k.x + k.y }.key
-        return find(northWestCorner).toSet()
+        return find(maze.northWestCorner).toSet()
     }
 
     /**
      * Enlarge maze by scaling it 2x to the east and then south,
      * followed by adding a ring of empty tiles around the scaled up maze.
      */
-    private fun enlargeMaze(): Map<Coordinate, Char> {
-        val startNode = maze.filterValues { it == 'S' }.keys.single()
-        val startValue = inferPipe(startNode, maze)
-        val enlargedMaze = maze.map { (orig, value) ->
-            val inferredValue = if (value == 'S') startValue else value
+    private fun enlargeMaze(input: Maze): Maze {
+        val enlargedMaze = input.tiles.map { (orig, value) ->
+            val actualValue = if (value == 'S') input.inferredStartValue else value
             val updated = Coordinate(2 * orig.x, orig.y)
             val added = Coordinate(updated.x + 1, updated.y)
-            when (inferredValue) {
+            when (actualValue) {
                 '│', '░', '┘', '┐' -> setOf(Pair(updated, value), Pair(added, '░'))
                 '─', '└', '┌' -> setOf(Pair(updated, value), Pair(added, '─'))
                 else -> throw RuntimeException("Invalid node value!")
             }
         }.flatten().toMap().map { (orig, value) ->
-            val inferredValue = if (value == 'S') startValue else value
+            val actualValue = if (value == 'S') input.inferredStartValue else value
             val updated = Coordinate(orig.x, 2 * orig.y)
             val added = Coordinate(updated.x, updated.y + 1)
-            when (inferredValue) {
+            when (actualValue) {
                 '─', '░', '┘', '└' -> setOf(Pair(updated, value), Pair(added, '░'))
                 '│', '┐', '┌' -> setOf(Pair(updated, value), Pair(added, '│'))
                 else -> throw RuntimeException("Invalid node value!")
@@ -133,21 +82,9 @@ class PipeMaze(resource: String = "day10/input.txt") : KtPuzzle(resource) {
             putAll((northWestCorner.y..(southEastCorner.y))
                 .map { Pair(Coordinate(southEastCorner.x + 1, it), '░') })
         }
-        return result.toMap()
+        return Maze(result.toMap())
     }
 
-    private fun print(maze: Map<Coordinate, Char>) {
-        val northWestCorner = maze.minBy { (k, _) -> k.x + k.y }.key
-        val southEastCorner = maze.maxBy { (k, _) -> k.x + k.y }.key
-        for (y in northWestCorner.x..southEastCorner.y) {
-            var line = ""
-            for (x in northWestCorner.y..southEastCorner.x) {
-                line += maze[Coordinate(x, y)] ?: ' '
-            }
-            println(line)
-        }
-        println()
-    }
 }
 
 fun main() {
